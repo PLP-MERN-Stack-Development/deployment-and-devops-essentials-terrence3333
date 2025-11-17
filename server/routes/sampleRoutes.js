431f8a1f68d806ab37b4745a0fab0b78c1aa3174
Terrence Ng'andu <nganduterrence0@gmail.com>
2025-11-17 20:04:01 +0200

    Create sampleRoutes.js

---
+++ server/routes/sampleRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getMessage } from "../controllers/sampleController.js";
+
+const router = express.Router();
+
+router.get("/", getMessage);
+
+export default router;
